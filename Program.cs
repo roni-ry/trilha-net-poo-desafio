7f3dc13cc6112fe39f152543e4f9b3cb915b5b5e
Ronald Teixeira <ronaldrt07@gmail.com>
2025-06-21 11:20:45 -0300

    Implementação da herança e da sobrescreção do metodo InstalalarAplicativo nas classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine($"Smartphone Nokia:");
+Smartphone nokia = new Nokia(
+    "00 - 00000 - 0000", "Modelo 1", "00000-00000-00000-00000", 12);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine($"Smartphone Iphone:");
+
+Smartphone iphone = new Iphone(
+    "10 - 10000 - 1000", "Modelo 2", "11111-11111-11111-11111", 10);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
